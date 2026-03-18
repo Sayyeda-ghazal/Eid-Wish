f7b18c00cbdd989f8bcb6835143d7b6fbcf24964
Sayyeda Ghazal <syedaghazalzehra89@gmail.com>
2026-03-18 17:09:31 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    sender_name = request.args.get("from", "").strip()
+    if not sender_name:
+        sender_name = "Your Name"
+    return render_template("index.html", sender_name=sender_name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
